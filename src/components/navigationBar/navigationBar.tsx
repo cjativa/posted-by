83b8bd7932d450efaee17c54319bb6acfe8adc02
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-06-15 03:20:09 -0400

    better positioning on page

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -1,10 +1,12 @@
 
 export const NavigationBar = () => {
     return (
-        <div className="p-4">
-            <ul className="flex">
+        <div className="p-2">
+
+            <ul className="flex flex-row items-center">
+
                 <li className="mr-6">
-                    <span className="text-blue-500">Posts.by</span>
+                    <span className="text-blue-500">Ideas.by</span>
                 </li>
                 <li className="mr-6">
                     <a className="text-blue-500 hover:text-blue-800" href="#">Link</a>
@@ -14,7 +16,7 @@ export const NavigationBar = () => {
                 </li>
                 <li className="mr-6">
                     <img
-                        className="inline object-cover w-12 h-12 mr-2 rounded-full"
+                        className="inline object-cover w-10 h-10 mr-2 rounded-full"
                         src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
                         alt="Profile image" />
                 </li>
