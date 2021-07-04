ee62575c6e888b10e0d056b587ffa0bef88c8272
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-07-03 20:35:54 -0400

    display posts for a thread post

--- src/features/user/post/post.tsx
+++ src/features/user/post/post.tsx
@@ -2,6 +2,7 @@ import { IThread } from '../../../../shared/thread';
 import { IUser } from '../../../../shared/user';
 import { Block } from '../../../components/block/block';
 import { UserLayout } from '../userLayout/userLayout';
+import { Heading } from '../../../components/heading/heading';
 
 export interface IFullPostProps {
     post: IThread,
@@ -9,13 +10,34 @@ export interface IFullPostProps {
 
 export const FullPost = ({ post }: IFullPostProps) => {
 
+    console.log(post.thread_posts);
+
     return (
         <UserLayout>
 
             <div className="w-7/12">
                 <Block>
-                    <div className=" grid grid-cols-1 gap-y-4">
-                        Thread contents
+                    <div className=" grid grid-cols-1">
+
+                        <Heading>
+                            {post.title}
+                        </Heading>
+
+                        {/** Display each post in the thread */}
+                        {post.thread_posts &&
+                            post.thread_posts.map((threadPost) => {
+                                return (
+                                    <Block paddingX={16} paddingY={8}>
+
+                                        <div
+                                            className="w-7/12 whitespace-pre-line "
+                                            dangerouslySetInnerHTML={{ __html: threadPost.text }}
+                                        />
+
+                                    </Block>
+                                )
+                            })
+                        }
 
                     </div>
                 </Block>
