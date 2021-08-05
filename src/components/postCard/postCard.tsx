410cae340e68bb9228321f1e94e2200ecf8e52d7
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-08-04 21:52:43 -0400

    better date formatting, display author information when available

--- src/components/postCard/postCard.tsx
+++ src/components/postCard/postCard.tsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from 'react';
+
 import { IThread } from '../../../shared/thread';
+import { Avatar } from '../avatar/avatar';
 
 interface IPostCardProps {
     post: IThread,
+    link: string,
+
+    authorHandle?: string,
+    authorName?: string,
+    authorImageUrl?: string,
 }
 
-export const PostCard = ({ post }: IPostCardProps) => {
+const lineStyle = {
+    borderTop: '1px solid red',
+    width: '60%',
+    height: '2px',
+    marginLeft: '10px',
+};
+
+export const PostCard = ({ post, link, authorImageUrl, authorHandle, authorName, }: IPostCardProps) => {
 
     const [shortDescription, setShortDescription] = useState('');
+    const [shortDate, setShortDate] = useState('');
 
     // Capitalize the first letter in the title
     const title = `${post.title.charAt(0).toUpperCase()}${post.title.slice(1)}`;
+
 
     useEffect(() => {
 
@@ -21,9 +37,17 @@ export const PostCard = ({ post }: IPostCardProps) => {
             const titleEnd = initialTweet.indexOf(title) + title.length;
             const description = initialTweet
                 .substring(titleEnd)
-                // .replaceAll('\n', '. ');
+            // .replaceAll('\n', '. ');
             setShortDescription(description);
         }
+
+        // Make the date into a short format like "April 21, 2021"
+        const date = new Date(post.created_at);
+        const month = date.toLocaleString('default', { month: 'long' });
+        const dateFormatted = `${month} ${date.getDate()}, ${date.getFullYear()}`;
+
+        setShortDate(dateFormatted);
+
     }, []);
 
 
@@ -31,43 +55,51 @@ export const PostCard = ({ post }: IPostCardProps) => {
     return (
         <div className={``}>
 
-            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg lg:w-80 shadow-lg">
+            <div className="flex flex-col h-full border-2 border-gray-200 border-opacity-60 rounded-lg lg:w-80 shadow-lg">
+
+                {/** Upper section - color cover */}
                 <div className={`lg:h-48 md:h-36 w-full bg-${post.theme_color}-400`} />
 
-                {/* <img
-                    className="lg:h-48 md:h-36 w-full object-cover object-center"
-                    src="https://dummyimage.com/720x400"
-                    alt="blog"
-                /> */}
+                {/** Bottom section */}
+                <a href={link} className="p-6 cursor-pointer block flex-grow">
+                    <div className="flex flex-col h-full">
+                        <div className="flex flex-row items-center mb-4">
+                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                                Category
+                            </h2>
+                            <hr style={lineStyle} />
+                        </div>
 
-                <div className="p-6">
-                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                        {post.created_at}
-                    </h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">
+                            {title}
+                        </h1>
 
-                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                        {title}
-                    </h1>
+                        <p className="leading-relaxed mb-3 flex-grow">
+                            {shortDescription}
+                        </p>
 
-                    <p className="leading-relaxed mb-3">
-                        {shortDescription}
-                    </p>
+                        <div className="flex flex-row gap-x-4 justify-between">
+                            <span className="tracking-widest text-xs title-font font-medium text-gray-400 self-end">
+                                {shortDate}
+                            </span>
+                            {authorImageUrl && authorHandle && authorName &&
+                                <div className="inline-flex gap-x-2">
+                                    <Avatar
+                                        imageSrc={authorImageUrl}
+                                        size={10}
+                                    />
 
-                    {/* <div className="flex items-center flex-wrap ">
-                        <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                            Learn More
-                        </a>
+                                    <div className="self-end">
+                                        <p className="font-bold text-sm">{authorName}</p>
+                                        <p className="text-sm">@{authorHandle}</p>
+                                    </div>
+                                </div>
+                            }
+                        </div>
 
-                        <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                <circle cx="12" cy="12" r="3"></circle>
-                            </svg>1.2K
-                        </span>
-                    </div> */}
-
-                </div>
+                    </div>
+                </a>
             </div>
-        </div>
+        </div >
     );
 };
