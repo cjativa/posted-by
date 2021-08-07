13d87f08d596c997fb18cbb35d82d58e012468de
Christopher D Jativa <cjativa@barnesfoundation.org>
2021-08-06 23:25:39 -0400

    better orientation for user snippet

--- src/components/userSnip/userSnip.tsx
+++ src/components/userSnip/userSnip.tsx
@@ -4,23 +4,40 @@ interface IUserSnipProps {
     imageUrl: string,
     handle: string,
     name: string,
+    orientation?: 'portrait' | 'landscape',
 };
 
-export const UserSnip = ({ imageUrl, handle, name }: IUserSnipProps) => {
+export const UserSnip = ({ imageUrl, handle, name, orientation = 'landscape' }: IUserSnipProps) => {
 
+    if (orientation === 'portrait') {
+        return (
+            <div className="grid grid-cols-1" >
+                <div className="w-24 h-24 relative mb-4">
+                    <Avatar
+                        imageSrc={imageUrl}
+                        alt={`Profile image for ${handle}`}
+                    />
+                </div>
 
+                <p className="font-bold">{name}</p>
+                <span>@{handle}</span>
+            </div>
+        );
+    }
 
     return (
-        < div className="grid grid-cols-1" >
-            <div className="w-24 h-24 relative mb-4">
-                <Avatar
-                    imageSrc={imageUrl}
-                    alt={`Profile image for ${handle}`}
-                />
-            </div>
+        <div className="flex flex-row items-end gap-x-4">
+            <Avatar
+                imageSrc={imageUrl}
+                alt={`Profile image for ${handle}`}
+                size={16}
+            />
 
-            <p className="font-bold">{name}</p>
-            <span>@{handle}</span>
+
+            <div className="flex flex-col justify-between">
+                <p className="">{name}</p>
+                <a href={`/${handle}`} className="text-blue-400">@{handle}</a>
+            </div>
         </div >
     );
 };
